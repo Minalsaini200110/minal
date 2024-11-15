7515bd775004a2cbf80ad695a49dd8ebf81adde0
Minalsaini200110 <minalsaini1003@gmail.com>
2024-11-15 14:55:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.1.0.0/16"
 
   tags = {
     Name = "MyVPC"
@@ -12,7 +12,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 
